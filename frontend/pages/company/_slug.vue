cb1ed11a60ae4f8f81bbda5ad39ff7c02acaa8f2
keishi <caseytea0840@gmail.com>
2021-11-01 16:16:21 +0800

    listing pages, company/developer own profiles

---
+++ frontend/pages/company/_slug.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Nav :class="bgcolor" />
+    <PageHeader :header="header" :class="bgcolor" />
+    <PageBody :body="body">
+      <b-container class="text-center">
+        <!-- Profile pic here -->
+        <h3 class="mb-4">{{ company.companyName }}</h3>
+         <h4 class="mb-4 font-italic">{{ company.companyTagline }}</h4>
+        <p>
+          {{ company.companyDescription }}
+        </p>
+        <p>Email: {{ company.companyEmail }}</p>
+        <p>Website: {{ company.companyWebsite }}</p>
+        <!-- Jobs posted here -->
+      </b-container>
+    </PageBody>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bgcolor: 'bg-nf-secondary',
+      header: {
+        title: 'Explore Comapnies',
+      },
+      body: {
+        title: ''
+      }
+    }
+  },
+  async asyncData({ params, $axios }) {
+    const company = await $axios.$get(`http://localhost:8000/company/${params.slug}`)
+    return { company }
+  }
+}
+</script>
---
+++ frontend/pages/developer/_slug.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Nav :class="bgcolor" />
+    <PageHeader :header="header" :class="bgcolor" />
+    <PageBody :body="body">
+      <b-container class="text-center">
+        <!-- Profile pic here -->
+        <h3 class="mb-4">{{ developer.developerFirstName }} {{ developer.developerLastName }}</h3>
+         <h4 class="mb-4">{{ developer.developerProfessionalTitle }}</h4>
+        <p>
+          {{ developer.developerDescription }}
+        </p>
+        <p>Email: {{ developer.developerLastName }}</p>
+        <p>Website: {{ developer.developerWebsite }}</p>
+        <!-- get pdf here -->
+      </b-container>
+    </PageBody>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bgcolor: 'bg-nf-primary',
+      header: {
+        title: 'Explore Developers',
+      },
+      body: {
+        title: ''
+      }
+    }
+  },
+  async asyncData({ params, $axios }) {
+    const developer = await $axios.$get(`http://localhost:8000/developer/${params.slug}`)
+    return { developer }
+  }
+}
+</script>
